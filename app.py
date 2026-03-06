f7142b17ac659a16fed9eac90441a46e172cbefa
Amritha2303 <25sbca007@psgrkcw.ac.in>
2026-03-06 17:15:57 +0530

    back toactual code

--- app.py
+++ app.py
@@ -1,42 +1,32 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
 
-# DATABASE CONNECTION
 def get_db():
     conn = sqlite3.connect("database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
-
-# LOGIN PAGE
 @app.route("/")
 def home():
     return render_template("login.html")
 
-
-# LOGIN CHECK
 @app.route("/login", methods=["POST"])
 def login():
 
-    child_id = request.form["health_id"]   # value entered by parent
+    health_id = request.form["health_id"]
 
     conn = get_db()
-
     child = conn.execute(
-        "SELECT * FROM children WHERE child_id = ?",
-        (child_id,)
+        "SELECT * FROM children WHERE health_id = ?",
+        (health_id,)
     ).fetchone()
-
-    conn.close()
 
     if child:
         return render_template("dashboard.html", child=child)
     else:
         return "Child not found"
 
-
-# RUN SERVER
 if __name__ == "__main__":
     app.run(debug=True)
